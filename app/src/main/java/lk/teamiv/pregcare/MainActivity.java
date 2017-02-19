226969c382337c2689b9a55c584221d78629f365
Sajith <sajithdherath@gmail.com>
2017-02-19 19:36:00 +0530

    Initial commit

---
+++ app/src/main/java/lk/teamiv/pregcare/MainActivity.java
@@ -0,0 +1,92 @@
+package lk.teamiv.pregcare;
+
+
+import android.bluetooth.BluetoothAdapter;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    public final String TAG = "Main";
+    private Bluetooth bt;
+    private  TextView status;
+    TextView temp;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        status = (TextView) findViewById(R.id.txtstatus);
+        temp= (TextView) findViewById(R.id.temp);
+        genVal();
+    }
+
+    public void genVal(){
+        TextView air = (TextView) findViewById(R.id.air);
+        TextView ecg = (TextView) findViewById(R.id.ecg);
+        //ecg.setText("Normal");
+        TextView pulse = (TextView) findViewById(R.id.pulse);
+        //pulse.setText("81");
+        //temp = (TextView) findViewById(R.id.temp);
+        //temp.setText(bt.getMsgTemp());
+        bt = new Bluetooth(this,mHandler);
+        connectService();
+
+
+
+
+}
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case Bluetooth.MESSAGE_STATE_CHANGE:
+                    Log.d(TAG, "MESSAGE_STATE_CHANGE: " + msg.arg1);
+                    break;
+                case Bluetooth.MESSAGE_WRITE:
+                    Log.d(TAG, "MESSAGE_WRITE ");
+                    break;
+                case Bluetooth.MESSAGE_READ:
+                    Log.d(TAG, "MESSAGE_READ ");
+                    //Extract the string from the Message
+                    String message = (String) msg.obj;
+                    temp.setText(message+"\u00b0"+"C");
+                    break;
+                case Bluetooth.MESSAGE_DEVICE_NAME:
+                    Log.d(TAG, "MESSAGE_DEVICE_NAME "+msg);
+                    break;
+                case Bluetooth.MESSAGE_TOAST:
+                    Log.d(TAG, "MESSAGE_TOAST "+msg);
+                    break;
+            }
+        }
+    };
+    public void connectService(){
+        try {
+            status.setText("Connecting...");
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            if (bluetoothAdapter.isEnabled()) {
+                bt.start();
+                bt.connectDevice("HC-06");
+                Log.d(TAG, "Btservice started - listening");
+                status.setText("Connected");
+
+            } else {
+                Log.w(TAG, "Btservice started - bluetooth is not enabled");
+                status.setText("Bluetooth Not enabled");
+            }
+        } catch(Exception e){
+            Log.e(TAG, "Unable to start bt ",e);
+            status.setText("Unable to connect " +e);
+        }
+    }
+}
---
+++ app/src/main/java/lk/teamiv/pregcare/SplashScreen.java
@@ -0,0 +1,32 @@
+package lk.teamiv.pregcare;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+}
