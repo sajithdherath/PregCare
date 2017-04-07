3aae6437774f9c7fa058c9133edde234c5aa16f9
Sajith <sajithdherath@gmail.com>
2017-04-07 22:01:21 +0530

    azuer fixed

--- app/src/main/java/lk/teamiv/pregcare/TodoItem.java
+++ app/src/main/java/lk/teamiv/pregcare/TodoItem.java
@@ -4,6 +4,7 @@ package lk.teamiv.pregcare;
  * Created by Sajith Deshappriya on 4/7/2017.
  */
 
-public class LoginActivity {
-
+public class TodoItem {
+    public String Id;
+    public String Text;
 }
